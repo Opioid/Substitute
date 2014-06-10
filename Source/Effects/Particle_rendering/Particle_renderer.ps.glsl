36f31aa12e0c4234bd5c125bd15435f1ff23b3e4
beni <benjamin.savs@gmail.com>
2014-06-10 15:28:48 +0200

    Experimental two-sided lighting for particles

--- Source/Effects/Particle_rendering/Particle_renderer.ps.glsl
+++ Source/Effects/Particle_rendering/Particle_renderer.ps.glsl
@@ -12,6 +12,37 @@ float soft_fade(float scene_depth, float particle_depth)
 	return (d > 0.5) ? 1.f - fade : fade;	
 }
 */
+
+vec3 ambient_cube_lighting(vec3 normal_ws, vec3 ambient_cube[6])
+{
+	vec3 normal_squared = normal_ws * normal_ws; 
+	
+	int is_negative_x = normal_ws.x < 0.f ? 1 : 0;
+	int is_negative_y = normal_ws.y < 0.f ? 1 : 0;
+	int is_negative_z = normal_ws.z < 0.f ? 1 : 0;
+	
+	return normal_squared.x * ambient_cube[is_negative_x]     
+		 + normal_squared.y * ambient_cube[is_negative_y + 2] 
+		 + normal_squared.z * ambient_cube[is_negative_z + 4]; 
+}
+
+vec3 two_sided_ambient_cube_lighting(vec3 normal_ws, float translucency, vec3 ambient_cube[6])
+{
+	vec3 normal_squared = normal_ws * normal_ws; 
+	
+	int is_negative_x = normal_ws.x < 0.f ? 1 : 0;
+	int is_negative_y = normal_ws.y < 0.f ? 1 : 0;
+	int is_negative_z = normal_ws.z < 0.f ? 1 : 0;
+	
+	int is_positive_x = -normal_ws.x < 0.f ? 1 : 0;
+	int is_positive_y = -normal_ws.y < 0.f ? 1 : 0;
+	int is_positive_z = -normal_ws.z < 0.f ? 1 : 0;
+
+	return normal_squared.x * (ambient_cube[is_negative_x]     + translucency * ambient_cube[is_positive_x])
+		 + normal_squared.y * (ambient_cube[is_negative_y + 2] + translucency * ambient_cube[is_positive_y + 2])
+		 + normal_squared.z * (ambient_cube[is_negative_z + 4] + translucency * ambient_cube[is_positive_z + 4]); 
+}
+
 void main()
 {
 #ifdef ARRAY
@@ -23,33 +54,6 @@ void main()
 
 	float alpha = pixel_in.tex_coord.z;
 #endif // ARRAY	
-
-#ifdef LIGHTING
-
-	vec3 ambient_cube[6] = { pixel_in.cube_0.xyz,
-							 vec3(pixel_in.cube_0.w,  pixel_in.cube_1.xy),
-							 vec3(pixel_in.cube_1.zw, pixel_in.cube_2.x),
-							 pixel_in.cube_2.yzw,
-							 pixel_in.cube_3.xyz,
-							 vec3(pixel_in.cube_3.w, pixel_in.cube_4.xy) };
-
-	vec3 normal_vs = normalize(pixel_in.normal_vs);
-
-	vec3 normal_ws = (vec4(normal_vs, 0.f) * g_view).xyz;						 
-
-	vec3 normal_squared = normal_ws * normal_ws; 
-	
-	int is_negative_x = normal_ws.x < 0.f ? 1 : 0;
-	int is_negative_y = normal_ws.y < 0.f ? 1 : 0;
-	int is_negative_z = normal_ws.z < 0.f ? 1 : 0;
-	
-	vec3 lighting = normal_squared.x * ambient_cube[is_negative_x]     
-		          + normal_squared.y * ambient_cube[is_negative_y + 2] 
-		          + normal_squared.z * ambient_cube[is_negative_z + 4]; 
-
-	color.rgb = lighting * color.rgb;
-
-#endif // LIGHTING
 
 #ifdef SOFT
 	ivec2 pixel_coord = ivec2(gl_FragCoord.x, gl_FragCoord.y);
@@ -64,6 +68,31 @@ void main()
 #endif // SOFT
 
 	color.a *= alpha;
+
+#ifdef LIGHTING
+
+	vec3 ambient_cube[6] = { pixel_in.cube_0.xyz,
+							 vec3(pixel_in.cube_0.w,  pixel_in.cube_1.xy),
+							 vec3(pixel_in.cube_1.zw, pixel_in.cube_2.x),
+							 pixel_in.cube_2.yzw,
+							 pixel_in.cube_3.xyz,
+							 vec3(pixel_in.cube_3.w, pixel_in.cube_4.xy) };
+
+	vec3 normal_vs = normalize(pixel_in.normal_vs);
+
+	vec3 normal_ws = (vec4(normal_vs, 0.f) * g_view).xyz;						 
+
+	/*
+	float translucency = 1.f - color.a;
+
+	vec3 lighting = two_sided_ambient_cube_lighting(normal_ws, translucency, ambient_cube);
+	*/
+
+	vec3 lighting = ambient_cube_lighting(normal_ws, ambient_cube);
+
+	color.rgb = lighting * color.rgb;
+
+#endif // LIGHTING
 
 	pixel_out.color = color;
 }
